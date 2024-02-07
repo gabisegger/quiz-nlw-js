f4d5964e3f978922fa2a39bf571c2a22a6eaa0a3
gabisegger <gabi.segger@gmail.com>
2024-02-07 09:31:25 -0300

    Arquivos iniciais do projeto

---
+++ script.js
@@ -0,0 +1,92 @@
+const perguntas = [
+  {
+    pergunta: "O que significa WCAG?",
+    resposta: [
+      "World Consortium of Accessibility Guidelines",
+      "Web Content Accessibility Guidelines",
+      "Website Care Accessibility Group",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é a principal função do alt atributo em uma tag de imagem?",
+    resposta: [
+      "Definir a posição da imagem na página",
+      "Descrever o conteúdo da imagem para pessoas que utilizam leitores de tela",
+      "Alterar a cor da imagem",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual das seguintes opções é uma boa prática para garantir a acessibilidade de um site para pessoas com deficiência visual?",
+    resposta: [
+      "Usar apenas texto em vez de imagens",
+      "Utilizar cores vibrantes e contrastantes para atrair a atenção",
+      "Incluir descrições textuais em imagens usando o atributo alt",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Por que é importante manter uma estrutura HTML semanticamente correta para a acessibilidade web?",
+    resposta: [
+      "Porque uma estrutura HTML complexa torna o site mais interessante visualmente",
+      "Porque uma estrutura semanticamente correta ajuda os leitores de tela a entender a organização e o significado do conteúdo",
+      "Porque uma estrutura HTML não tem impacto na acessibilidade",
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é a prática recomendada para garantir que formulários sejam acessíveis?",
+    resposta: [
+      "Usar apenas campos de texto",
+      "Utilizar apenas menus suspensos para seleção de opções",
+      "Incluir rótulos descritivos para cada campo de formulário",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Como as pessoas com deficiência visual geralmente navegam em uma página da web?",
+    resposta: [
+      "Clicando em links aleatoriamente",
+      "Usando um cursor de mouse",
+      "Utilizando leitores de tela",
+    ],
+    correta: 3
+  },
+  {
+    pergunta: "O que é contraste de cores e por que é importante para a acessibilidade web?",
+    resposta: [
+      "É a quantidade de cores diferentes usadas em uma página, o que torna o site mais atraente",
+      "É a diferença entre a luminosidade das cores de primeiro e segundo plano, importante para tornar o texto legível para pessoas com baixa visão",
+      "Não tem impacto na acessibilidade",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual das seguintes opções não é uma diretriz de acessibilidade para vídeos?",
+    resposta: [
+      "Incluir legendas e transcrições para conteúdo de áudio",
+      "Garantir que os vídeos tenham uma resolução mínima de 4K",
+      "Fornecer controles de reprodução de mídia acessíveis",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Por que é importante testar a acessibilidade de um site em diferentes navegadores e dispositivos?",
+    resposta: [
+      "Porque todos os navegadores e dispositivos renderizam páginas da web exatamente da mesma maneira",
+      "Porque diferentes navegadores e dispositivos podem interpretar o código de maneira diferente, afetando a acessibilidade",
+      "Porque a acessibilidade não é afetada por diferentes navegadores ou dispositivos",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que é uma característica importante de um site acessível para pessoas com deficiência motora?",
+    resposta: [
+      "Muitos elementos interativos que requerem movimentos precisos do mouse",
+      "Teclado navegável e navegação por voz",
+      "Navegação exclusivamente por gestos de toque",
+    ],
+    correta: 2
+  },
+];
